7633fc0b045a62d302a31a871663bb8f90c5c5d8
Kirill <121577005+Communinst@users.noreply.github.com>
2023-11-28 20:34:33 +0300

    Done.

--- Lab47.c
+++ Lab47.c
@@ -20,6 +20,11 @@ int main (int argc, char** argv)
             printf("SUCCESS!\n");
             break;
 
+        case INVALID:
+
+            printf("Some variable is not defined!\n");
+            break;
+
         default:
             printf("IDIOT!\n");
 
@@ -28,6 +33,7 @@ int main (int argc, char** argv)
     return 0;
 
 }
+
 
 
 EXIT_CODE cell_constr (memory_cell** dest, char* name, ll value)
@@ -117,13 +123,32 @@ EXIT_CODE vec_append (memory_vec* dest, memory_cell* to_add)
 
     dest->occupied++;
 
+    qsort(dest->cells, dest->occupied, sizeof(memory_cell*), lex_cmp);
+
     return OK;
+
+}
+
+
+int lex_cmp (const void* v1, const void* v2)
+{
+
+    memory_cell** c1 = (memory_cell**)v1;
+    memory_cell** c2 = (memory_cell**)v2;
+
+    return (strcmp((*c1)->name, (*c2)->name));
+
 
 }
 
 
 EXIT_CODE vec_display (memory_vec* src)
 {
+
+    if (!src->occupied)
+    {
+        return OK;
+    }
 
     memory_cell** crawler = src->cells;
 
@@ -149,7 +174,7 @@ EXIT_CODE vec_destr (memory_vec** dest)
     while (((*dest)->occupied)--)
     {
 
-        cell_destr(*crawler);
+        cell_destr(&(*crawler));
 
         crawler++;
 
@@ -169,7 +194,7 @@ EXIT_CODE input_handle (int argc, char** argv)
         return INVALID;
     }
 
-    data_handle(*(argv + 1));
+    return data_handle(*(argv + 1));
 
 }
 
@@ -186,7 +211,7 @@ EXIT_CODE data_handle (char* filename)
         return assist;
     }
 
-    assist = data_handle(in);
+    assist = memory_hub(in);
 
     fclose(in);
 
@@ -215,15 +240,20 @@ EXIT_CODE file_handle (char* src, FILE** dest)
 EXIT_CODE memory_hub (FILE* src)
 {
 
-    memory_vec* memory = NULL;
+    memory_vec* memory = (memory_vec*)malloc(sizeof(memory_vec));
+
+    if (!memory)
+    {
+        return BAD_ALLOC;
+    }
 
     vec_init(&memory);
 
-
-    fill_vec(memory, src);
-
+    EXIT_CODE assist = fill_vec(memory, src);
 
     vec_destr(&memory);
+
+    return assist;
 
 }
 
@@ -233,44 +263,72 @@ EXIT_CODE fill_vec (memory_vec* dest, FILE* src)
 
     char c = 1;
 
+    EXIT_CODE assist;
+
     while (c > 0)
     {
 
         memory_cell* unit = NULL;
 
-        get_cell(&unit, src, &c);
+        assist = get_cell(&unit, dest, src, &c);
 
-        vec_append(dest, unit);
+        switch(assist)
+        {
+
+            case APPEND:
+
+                vec_append(dest, unit);
+                break;
+
+            case INVALID:
+
+                return INVALID;
+                break;
+
+            default:
+                break;
+                
+        }
 
     }
 
-
+    return OK;
 
 }
 
 
-EXIT_CODE get_cell(memory_cell** dest, FILE* src, char *c)
+EXIT_CODE get_cell (memory_cell** dest, memory_vec* base, FILE* src, char* c)
 {
 
-    char* name;
+    char* name = NULL;
 
-    get_variable(&name, src, &c);
+    ll value = 0;
 
-    OPERATION op = operation_handle(c);
+    get_variable(&name, src, c);
 
-    if (op == NEXT)
+    OPERATION op = op_handle(*c);
+
+    if (check_op(&value, src, c, base) == INVALID)
     {
-        handle_print();
+        return INVALID;
     }
 
-    if (!op)
+    *c = fgetc(src);
+
+    if (op != NEXT && op != SKIP && dichotomy_rewrite(base, value, name) == NONE)
     {
 
-        ll value = 0;
+        cell_constr(dest, name, value);
 
-        complete_inst(src, c, &value);
+        free(name);
+
+        return APPEND;
 
     }
+
+    free(name);
+
+    return OK;
 
 }
 
@@ -285,19 +343,16 @@ EXIT_CODE get_variable (char** dest, FILE* src, char* c)
         return BAD_ALLOC;
     }
 
-    char* crawler = buff;
-
-    *crawler = '\0';
+    buff[0] = '\0';
 
     int amount = 0;
 
-    while (isalpha(*c = fgetc(src)) || isdigit(*c))
+    while ((op_handle(*c = fgetc(src)) == DEFAULT) && (*c > 0))
     {
 
         if (amount == strlen(buff))
         {
-
-            char* temp = (char*)realloc(buff, sizeof(char) * (amount++) * 2);
+            char* temp = (char*)realloc(buff, sizeof(char) * ((amount + 1) * 2));
 
             if (!temp)
             {
@@ -308,11 +363,8 @@ EXIT_CODE get_variable (char** dest, FILE* src, char* c)
 
         }
 
-        *crawler = *c;
-
-        crawler++;
-
-        *crawler = '\0';
+        buff[amount] = *c;
+        buff[++amount] = '\0';
 
     }
 
@@ -323,59 +375,302 @@ EXIT_CODE get_variable (char** dest, FILE* src, char* c)
 }
 
 
-OPERATION operation_handle (char c)
+OPERATION op_handle (char c)
 {
 
-    if (c < 0 || c == '\n')
+    switch (c)
     {
-        return NEXT;
-    }
-    switch(c)
-    {
+
+        case ';':
+            return NEXT;
+
+        case ' ':
+            return SKIP;
 
         case '=':
             return ASSIGN;
-        
+
         case '+':
             return PLUS;
 
         case '-':
             return MINUS;
 
-        case '\\':
-            return DIVISION;
-
         case '*':
             return MULTIPLICITY;
 
+        case '/':
+            return DIVISION;
+
         case '%':
             return MODULUS;
-
+        
         default:
-            printf("UNKNOWN SYMBOL!\n");
-            return NEXT;
+            return DEFAULT;
 
     }
+
+}
+
+
+EXIT_CODE check_op (ll* value, FILE* in, char* c, memory_vec* base)
+{
+
+    switch(op_handle(*c))
+    {
+
+        case ASSIGN:
+            return fill_instr(value, in, c, base);
+
+        case SKIP:
+            return fill_print(in, c, base);
+
+        case NEXT:
+            return vec_display(base);
+
+    }
+
+}
+
+
+EXIT_CODE fill_instr(ll* value, FILE* src, char* c, memory_vec* base)
+{
+
+    ll var1 = 0;
+    ll var2 = 0;
+    OPERATION op = ASSIGN;
+
+    char* v1 = NULL;
     
+    get_variable(&v1, src, c);
+
+    EXIT_CODE assist = identify_var(v1, &var1, base);
+
+    op = op_handle(*c);
+
+    free(v1);
+
+    if (op != NEXT)
+    {
+
+        get_variable(&v1, src, c);
+
+        assist = identify_var(v1, &var2, base);
+
+        free(v1);
+
+    }
+
+    return execute_op(op, var1, var2, value);
+
+}
+
+
+
+
+EXIT_CODE identify_var (char* str, ll* dest, memory_vec* base)
+{
+
+    if (!str)
+    {
+        return OK;
+    }
+
+    if (!is_num(str, dest))
+    {
+        return OK;
+    }
+
+    else 
+    {
+        if (dichotomy_take(base, dest, str) == INVALID)
+        {
+            return INVALID;
+        }
+
+        return OK;
+    }
+
+}
+
+
+EXIT_CODE is_num(char* name, ll* dest)
+{
+
+    char* end_ptr;
+
+    *dest = strtol(name, &end_ptr, 10);
+
+    return (end_ptr == name + strlen(name)) ? OK : INVALID;
 
 
 }
 
 
-EXIT_CODE complete_instr (FILE* src, char* c, ll* value)
+EXIT_CODE dichotomy_take (memory_vec* base, ll* dest, char* name)
 {
 
-    char* data;
+    if (!(base->occupied))
+    {
+        return INVALID;
+    }
 
-    while (*c != '\n' && *c > 0)
+    ll left = 0;
+    ll right = base->occupied;
+
+    ll mid = 0;
+
+    do
     {
 
-        get_variable(&data, src, c);
+        mid = (left + right) / 2;
 
-        OPERATION op = operation_handle(*c);
+        if (!strcmp((base->cells[mid]->name), name))
+        {
 
-        
+            *dest = base->cells[mid]->value;
+            return OK;
 
+        }
+
+        if (strcmp((base->cells[mid]->name), name) < 0)
+        {
+            left = mid;
+        }
+
+        else 
+        {
+            right = mid;
+        }
+
+    } while (left < right);
+
+    return INVALID;
+
+}
+
+
+EXIT_CODE dichotomy_rewrite (memory_vec* base, ll value, char* name)
+{
+
+
+    if (!(base->occupied))
+    {
+        return NONE;
     }
+
+    ll left = 0;
+    ll right = base->occupied;
+
+    ll mid = 0;
+
+    do
+    {
+
+        mid = ((left + right) / 2);
+
+        if (!strcmp((base->cells[mid]->name), name))
+        {
+
+            base->cells[mid]->value = value;
+            return OK;
+
+        }
+
+        if (strcmp((base->cells[mid]->name), name) < 0)
+        {
+            left = mid;
+        }
+
+        else 
+        {
+            right = mid;
+        }
+
+    } while (left < right && mid != ((left + right) / 2));
+
+    return NONE;
+
+
+}
+
+
+EXIT_CODE execute_op (OPERATION op, ll v1, ll v2, ll* res)
+{
+
+    if (op == ASSIGN || op == NEXT)
+    {
+        *res = v1;
+
+        return OK;
+    }
+
+    switch (op)
+    {
+        
+        case PLUS:
+
+            *res = v1 + v2;
+            break;
+
+        case MINUS:
+
+            *res = v1 - v2;
+            break;
+
+        case MULTIPLICITY:
+
+            *res = v1 * v2;
+            break;
+
+        case DIVISION:
+
+            if (!v2)
+            {
+                return INVALID;
+            }
+
+            *res = v1 / v2;
+            break;
+
+        case MODULUS:
+
+            if (!v2)
+            {
+                return INVALID;
+            }
+
+            *res = v1 % v2;
+            break;
+
+        default:
+            break;
+    }
+
+    return OK;
+
+}
+
+
+
+EXIT_CODE fill_print(FILE* src, char* c, memory_vec* base)
+{
+
+    char* name = NULL;
+
+    ll value = 0;
+
+    get_variable(&name, src, c);
+
+    if (dichotomy_take(base, &value, name) == INVALID)
+    {
+        return INVALID;
+    }
+
+    printf("%s = %lld\n\n", name, value);
+
+    free(name);
+
+    return OK;
 
 }
--- Procedure.h
+++ Procedure.h
@@ -17,6 +17,10 @@ typedef enum
 
     BAD_ALLOC,
 
+    NONE,
+
+    APPEND,
+
 } EXIT_CODE;
 
 
@@ -36,6 +40,12 @@ typedef enum
     MODULUS,
 
     NEXT,
+
+    SKIP,
+
+    END,
+
+    DEFAULT,
 
 
 } OPERATION;
@@ -65,6 +75,82 @@ typedef struct
     memory_cell** cells;
 
 } memory_vec;
+
+
+
+
+EXIT_CODE cell_constr (memory_cell** dest, char* name, ll value);
+
+
+EXIT_CODE cell_destr (memory_cell** dest);
+
+
+
+EXIT_CODE vec_init (memory_vec** dest);
+
+
+EXIT_CODE vec_append (memory_vec* dest, memory_cell* to_add);
+
+
+int lex_cmp (const void* v1, const void* v2);
+
+
+EXIT_CODE vec_display (memory_vec* src);
+
+
+EXIT_CODE vec_destr (memory_vec** dest);
+
+
+
+EXIT_CODE input_handle (int argc, char** argv);
+
+
+EXIT_CODE data_handle (char* filename);
+
+
+EXIT_CODE file_handle (char* src, FILE** dest);
+
+
+EXIT_CODE memory_hub (FILE* src);
+
+
+EXIT_CODE fill_vec (memory_vec* dest, FILE* src);
+
+
+EXIT_CODE get_cell (memory_cell** dest, memory_vec* base, FILE* src, char* c);
+
+
+EXIT_CODE get_variable (char** dest, FILE* src, char* c);
+
+
+OPERATION op_handle (char c);
+
+
+EXIT_CODE check_op (ll* value, FILE* in, char* c, memory_vec* base);
+
+
+EXIT_CODE fill_instr(ll* value, FILE* src, char* c, memory_vec* base);
+
+
+
+EXIT_CODE identify_var (char* str, ll* dest, memory_vec* base);
+
+
+EXIT_CODE is_num(char* name, ll* dest);
+
+
+EXIT_CODE dichotomy_take (memory_vec* base, ll* dest, char* name);
+
+
+EXIT_CODE dichotomy_rewrite (memory_vec* base, ll value, char* name);
+
+
+EXIT_CODE execute_op (OPERATION op, ll v1, ll v2, ll* res);
+
+
+
+EXIT_CODE fill_print(FILE* src, char* c, memory_vec* base);
+
 
 
 
